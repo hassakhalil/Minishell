bbc1f45c8f4c12e5d3e8ff4c382b9a663b023b5f
hassan khalil <hkhalil@student.1337.ma>
2022-08-13 22:24:19 +0100

    fixed one pipe case

--- executor.c
+++ executor.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 13:00:15 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/08/12 15:58:01 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/08/13 22:23:51 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 //how to execute struct *cmd
 //ask elyas about exec struct
 
-#define EXEC 1
+/*#define EXEC 1
 #define REDIR 2
-#define PIPE 3
+#define PIPE 3*/
 
 void errors(char *msg)
 {
@@ -30,64 +30,64 @@ void errors(char *msg)
 void    executor(cmd *result_tree, env *env)
 {
     int p[2];
-    int id1;
-    int id2;
+    int id;
     int open_fd;
     pip *tree1;
     redir *tree2;
     exec *tree3;
 
-    if (result_tree->type == PIPE)
+    if (result_tree->type == '|')
     {
         tree1 = (pip *)result_tree;
         if (pipe(p) < 0)
             errors("pipe error");
-        id1 = fork();
-        if (id1 < 0)
+        id = fork();
+        if (id < 0)
             errors("fork error");
-        if (id1 == 0)
+        if (id == 0)
         {
-            close(1);
-            dup(p[1]);
-            close(p[0]);
             close(p[1]);
-            executor(tree1->left);
+            dup2(p[0], 0);
+            close(p[0]);
+            executor(tree1->right,env);
         }
         else
         {
-            id2 = fork();
-            if (id2 < 0)
-                errors("fork error");
-            if (id2 == 0)
-            {
-                close(0);
-                dup(p[0]);
-                close(p[0]);
-                close(p[1]);
-                executor(tree1->right);
-            }
-        }
-        if (id1 && id2)
-        {
             close(p[0]);
+            dup2(p[1],1);
             close(p[1]);
-            wait();
+            executor(tree1->left, env);
+            wait(0);
         }
      }
-    else if (result_tree->type == REDIR)
+    else if (result_tree->type == '<')
     {
         tree2 = (redir *)result_tree;
-        open_fd = open(tree2->file, tree->mode);
+        open_fd = open(tree2->file, tree2->mode);
         if (open_fd < 0)
             errors("open error");
-        dup2(open_fd, tree->fd);
+        dup2(open_fd, tree2->fd);
         close(open_fd);
-        executor(tree2->cmd);
+        executor(tree2->cmd, env);
     }
     else
     {
+        printf("passed from heeeeeere\n");
         tree3 = (exec *)result_tree;
-        execve((tree3->argv)[0], tree3->argv, env->path);
-        errors("execve error");
-    }       
+       // execve((tree3->argv)[0], tree3->argv, env->path);
+       // errors("execve error");
+        //
+        char *p;
+        int i = -1;
+
+        while (env->path[++i])
+        {
+            p = ft_strjoin(ft_strjoin(env->path[i], "/"), tree3->argv[0]);
+            if (access(p, F_OK) != -1)
+            {
+                execve(p, tree3->argv, env->path);
+                return ;
+            }
+        }
+    }      
 }
