ef2fd0c9ddef509547960cfdbe33a3deb3119eee
hassan khalil <hkhalil@student.1337.ma>
2022-08-18 20:38:07 +0100

    fixing multiple pipes

--- executor.c
+++ executor.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 13:00:15 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/08/17 14:54:34 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/08/18 20:37:57 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 /*#define EXEC 1
 #define REDIR 2
 #define PIPE 3*/
+
 
 void errors(char *msg)
 {
@@ -38,6 +39,7 @@ void    executor(cmd *result_tree, env *env, int flag)
 
     if (result_tree->type == '|')
     {
+        write (2, "-----pipe-----\n", 16);
         tree1 = (pip *)result_tree;
         if (pipe(p) < 0)
             errors("pipe error");
@@ -47,19 +49,23 @@ void    executor(cmd *result_tree, env *env, int flag)
         if (id == 0)
         {
             close(p[0]);
-            dup2(p[1],1);
+            dup2(p[1], 1);
+            write (2, "-----pipe->left-----\n", 22);
+            executor(tree1->left,env, flag);
             close(p[1]);
-            executor(tree1->left, env, flag);
             return ;
         }
         close(p[1]);
         dup2(p[0], 0);
-        close(p[0]);
+        write (2, "-----pipe->right-----\n", 22);
         executor(tree1->right,env, flag);
+        close(p[0]);
         wait(0);
+        return ;
      }
-    else if (result_tree->type == '<')
+    else if (result_tree->type == '>')
     {
+        write (2, "-----redir-----\n", 17);
         tree2 = (redir *)result_tree;
         open_fd = open(tree2->file, tree2->mode);
         if (open_fd < 0)
@@ -67,16 +73,18 @@ void    executor(cmd *result_tree, env *env, int flag)
         dup2(open_fd, tree2->fd);
         close(open_fd);
         executor(tree2->cmd, env, flag);
+        return ;
     }
     else
     {
+        write (2, "-----exec-----\n", 16);
         tree3 = (exec *)result_tree;
-       // execve((tree3->argv)[0], tree3->argv, env->path);
-       // errors("execve error");
+        //printf("%s\n", env->path[6]);
+        //execve("/usr/local/bin/grep", tree3->argv, env->path);
+        //errors("execve error");
         //
         char *p;
         int i = -1;
-
         while (env->path[++i])
         {
             p = ft_strjoin(ft_strjoin(env->path[i], "/"), tree3->argv[0]);
@@ -86,5 +94,7 @@ void    executor(cmd *result_tree, env *env, int flag)
                 return ;
             }
         }
-    }      
+        
+        return ;
+    } 
 }
