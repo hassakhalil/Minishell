7bfd962ad309813d2c348bce8d9943cb4b3671b6
hassan khalil <hkhalil@student.1337.ma>
2022-08-19 15:07:45 +0100

    parsing tester

--- parsing_tester.c
+++ parsing_tester.c
@@ -1,51 +1,33 @@
 #include "minishell.h"
 
-void errors(char *msg)
+void    parsing_tester(cmd *result_tree)
 {
-    printf("%s\n", msg);
-    //clean everything
-    exit(1);
-}
-
-void    parsing_tester(cmd *result_tree, env *env, int flag)
-{
-    int id;
     pip *tree1;
     redir *tree2;
     exec *tree3;
 
     if (result_tree->type == '|')
     {
-        write (2, "-----pipe-----\n", 16);
+        write (2, "-----|pipe|-----\n", 18);
         tree1 = (pip *)result_tree;
-        id = fork();
-        if (id < 0)
-            errors("fork error");
-        if (id == 0)
-        {
-            write (2, "-----pipe->left-----\n", 22);
-            parsing_tester(tree1->left,env, flag);
-            return ;
-        }
-        write (2, "-----pipe->right-----\n", 22);
-        parsing_tester(tree1->right,env, flag);
-        wait(0);
-        return ;
-     }
+        write (2, "<<<<<<<<-----|pipe left|------\n", 32);
+        parsing_tester(tree1->left);
+        write (2, "-----|pipe right|----->>>>>>>>\n", 32);
+        if (tree1->right->type == ' ')
+            write(2, "+++++|last node|+++++\n", 23);
+        else if (tree1->right == 0)
+            write(2, "(((((((null node)))))))\n", 25);
+        parsing_tester(tree1->right);
+    }
     else if (result_tree->type == '>')
     {
-        write (2, "-----redir-----\n", 17);
+        write (2, "<><><>|redir|<><><>\n", 21);
         tree2 = (redir *)result_tree;
-        parsing_tester(tree2->cmd, env, flag);
-        return ;
+        parsing_tester(tree2->cmd);
     }
     else
     {
-        write (2, "-----exec-----\n", 16);
-        //printf("%s\n", env->path[6]);
-        //execve("/usr/local/bin/grep", tree3->argv, env->path);
-        //errors("execve error");
-        //
-        return ;
+        write (2, "|||-----exec-----|||\n", 22);
+        tree3 = (exec *)result_tree;
     } 
 }
