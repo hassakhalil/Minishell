e6b31418da88907a2a71b6e874a6700b34a87ec9
hassan khalil <hkhalil@student.1337.ma>
2022-09-10 15:23:36 +0100

    change some comments

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 11:34:23 by iakry             #+#    #+#             */
-/*   Updated: 2022/09/10 13:56:43 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/09/10 15:21:54 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,19 +63,20 @@ typedef struct execcmd
 } t_exec;
 
 
-// libft
+// utils
 char    *ft_strnstr(char	**big, char *little, size_t l);
 char	  **ft_split(char const *s, char c);
 char	  *ft_strjoin(char *s1, char *s2);
 char	  *ft_strdup(const char *str);
-
-// utils
 size_t  ft_strlen(char *s);
 char    *ft_strchr(const char *s, int c);
 int     forkk(void);
 char    *mkcopy(char *s, char *es);
 void    errors(char *msg);
-
+struct cmd* redircmd(struct cmd *subcmd, char *file, int mode, int type);
+struct cmd* pipecmd(struct cmd *left, struct cmd *right);
+struct cmd* execcmd(void);
+struct cmd* parsecmd(char *s);
 
 // path
 struct env *envpath(char **env);
@@ -84,27 +85,21 @@ char    *getpath(char *buff);
 // builtins
 int      cd(char *buff);
 
-// execution
+// executor
 void    executor(cmd *tree, env *envp, int *flag);
 void    parsing_tester(cmd *result_tree);
 int     check_in_files(cmd *first_redir);
-void     find_in_redir(cmd *tree, int *flag);
+void    find_in_redir(cmd *tree, int *flag);
 
-// lexer
+//parser
 struct cmd* parsepipe(char **ps, char *es);
 struct cmd* parsecmd(char *s);
 struct cmd* parseexec(char **ss, char *es);
 struct cmd* parseredirs(struct cmd *cmd, char **ss, char *es);
 
-// tokenizer
+//lexer
 int       gettoken(char **ps, char *es, char **q, char **eq);
 int       peek(char **ps, char *es, char *toks);
-
-// parser
-struct cmd* redircmd(struct cmd *subcmd, char *file, int mode, int type);
-struct cmd* pipecmd(struct cmd *left, struct cmd *right);
-struct cmd* execcmd(void);
-struct cmd* parsecmd(char *s);
 
 //signals
 void    handler(int sig);
