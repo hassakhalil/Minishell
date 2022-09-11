09dabf06d706b694af817bfeffd97139eba0ac9f
hassan khalil <hkhalil@student.1337.ma>
2022-09-11 15:44:52 +0100

    adding here doc

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 11:44:55 by iakry             #+#    #+#             */
-/*   Updated: 2022/09/11 15:25:54 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/09/11 15:39:24 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,8 @@ struct cmd* parseredirs(struct cmd *cmd, char **ss, char *es)
         if (tok == '*')
         {
             //here doc here
-            cmd = redircmd(cmd, mkcopy(q, eq), O_WRONLY|O_CREAT|O_TRUNC, tok);
+            //mkcopy(q,eq) is the dilimeter
+            cmd = redircmd(cmd, "heredocument", O_WRONLY|O_CREAT|O_TRUNC, tok);
             break;
         }
     }
