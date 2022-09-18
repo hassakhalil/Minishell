2043080cbf8d4d8b50d067d04885c4313434d231
Hassan Khalil <hkhalil@e2r2p9.1337.ma>
2022-09-18 21:40:52 +0100

    coding the expander

--- expander.c
+++ expander.c
@@ -12,7 +12,31 @@
 
 #include "minishell.h"
 
-void    expander()
+void    expander(t_cmd *tree, t_env *env)
 {
-    
+    t_pip     *tree1;
+    t_redir   *tree2;
+    t_exec    *tree3;
+
+    if (tree->type == PIPE)
+    {
+        tree1 = (t_pip *)tree;
+        expander(tree1->right, env);
+        expander(tree1->left, env);
+    }
+    else if (ree->type == REDIR)
+    {
+        tree2 = (t_redir *)tree;
+        expander(tree2->cmd, env);
+    }
+    else
+    {
+        while (/*there is a $x in argv[MAXAERGS]*/)
+        {
+            if (/* x == '?'*/)
+                //replace $x with GLOBAL value
+            else if (//search for x in env)
+                //replace $x with path[i];
+        }
+    }
 }
