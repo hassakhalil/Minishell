a39b50eb50ae464222ee69f38e5e900c48e91d9b
hassan khalil <hkhalil@student.1337.ma>
2022-10-14 20:55:28 +0100

    norm

--- errors.c
+++ errors.c
@@ -6,48 +6,57 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 20:13:17 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/10/14 20:13:31 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/14 20:54:47 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-extern int errno ;
+extern int	errno;
 
-void errors(char *name, int flag)
+void	errors1(char *name)
 {
-    if (flag == 4 && errno != 2)
-    {
-        if (errno == 22)
-        {
-            write(2, name, ft_strlen(name));
-            write(2, ": is a directory\n", 18);
-            exit(126);
-        }
-        perror(name);
-        exit(126);
-    }
-    if (flag == 2 || flag == 3)
-    {
-        write(2,name, ft_strlen(name));
-        if (flag == 2)
-            write(2, ": Permission denied\n", 21);
-        else
-             write(2, ": No such file or directory\n", 29);
-        exit(1);
-    }
-    if (errno == 2 || errno == 13)
-    {
-        if (ft_strchr(name, '/'))
-                perror(name);
-        else
-        {
-            write(2, name, ft_strlen(name));
-            write(2, ": command not found\n",21);
-        }
-        if (errno == 2)
-            exit(127);
-        else if (errno == 13)
-            exit(126);
-    } 
+	if (errno == 22)
+	{
+		write(2, name, ft_strlen(name));
+		write(2, ": is a directory\n", 18);
+		exit(126);
+	}
+	perror(name);
+	exit(126);
+}
+
+void	errors2(char *name, int flag)
+{
+	write(2, name, ft_strlen(name));
+	if (flag == 2)
+		write(2, ": Permission denied\n", 21);
+	else
+		write(2, ": No such file or directory\n", 29);
+	exit(1);
+}
+
+void	errors3(char *name)
+{
+	if (ft_strchr(name, '/'))
+		perror(name);
+	else
+	{
+		write(2, name, ft_strlen(name));
+		write(2, ": command not found\n", 21);
+	}
+	if (errno == 2)
+		exit(127);
+	else if (errno == 13)
+		exit(126);
+}
+
+void	errors(char *name, int flag)
+{
+	if (flag == 4 && errno != 2)
+		errors1(name);
+	if (flag == 2 || flag == 3)
+		errors2(name, flag);
+	if (errno == 2 || errno == 13)
+		errors3(name);
 }
