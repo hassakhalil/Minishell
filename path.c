75cd347998351e5560f7c968a4a18b84681c6b53
hassan khalil <hkhalil@student.1337.ma>
2022-10-14 05:44:45 +0100

    fixing norm

--- path.c
+++ path.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 13:07:20 by iakry             #+#    #+#             */
-/*   Updated: 2022/10/10 17:33:02 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/14 05:27:26 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,3 @@ char **envpath(t_envvar *env)
     }    
     return (path);
 }
-
-
-
-
-//TMPDIR SHELL HOME SSH_AUTH_SOCK Apple_PubSub_Socket_Render LOGNAME XPC_SERVICE_NAME COMMAND_MODE MallocNanoZone USER SECURITYSESSIONID XPC_FLAGS __CF_USER_TEXT_ENCODING ORIGINAL_XDG_CURRENT_DESKTOP SHLVL PWD OLDPWD HOMEBREW_CACHE HOMEBREW_TEMP TERM_PROGRAM TERM_PROGRAM_VERSION LANG COLORTERM GIT_ASKPASS VSCODE_GIT_ASKPASS_NODE VSCODE_GIT_ASKPASS_EXTRA_ARGS VSCODE_GIT_ASKPASS_MAIN VSCODE_GIT_IPC_HANDLE VSCODE_INJECTION ZDOTDIR TERM
-//_
--- quote_remover.c
+++ quote_remover.c
@@ -6,42 +6,36 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 05:09:26 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/10/02 19:27:57 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/14 05:43:35 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char    *quote_remover(char *s)
+char	*quote_remover(char *s)
 {
-    int i = 0;
-    int j = 0;
-    int c;
-    char    *new;
-    
-    new = malloc(sizeof(char) * ft_strlen(s) + 1);
-    while (s[i])
-    {
-        if (s[i] == '\'' || s[i] == '\"')
-        {
-            c = s[i];
-            i++;
-            while (s[i] && s[i] != c)
-            {
-                new[j] = s[i];
-                i++;
-                j++;
-            }
-            i++;
-        }
-        else
-        {
-            new[j] = s[i];
-            i++;
-            j++;
-        } 
-    }
-    new[j] = 0;
-    free(s);
-    return (new);
+	int		i;
+	int		j;
+	int		c;
+	char	*new;
+
+	i = 0;
+	j = 0;
+	new = malloc(sizeof(char) * ft_strlen(s) + 1);
+	while (s[i])
+	{
+		if (s[i] == '\'' || s[i] == '\"')
+		{
+			c = s[i++];
+			while (s[i] && s[i] != c)
+				new[j++] = s[i++];
+			if (s[i])
+				i++;
+		}
+		else
+			new[++j] = s[++i];
+	}
+	new[j] = 0;
+	free(s);
+	return (new);
 }
