39c9cf2b85a65fdd83103d4ffc018b4b76f0dde1
hassan khalil <hkhalil@student.1337.ma>
2022-10-15 01:52:14 +0100

    normed the executor function

--- exec.c
+++ exec.c
@@ -6,50 +6,73 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 01:14:08 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/10/15 01:17:20 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/15 01:47:08 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    ft_exec(t_cmd *tree, t_envvar **env_list)
+void	check_for_dir(char *s)
 {
-    DIR *dir;
-    int     i;
-    t_exec    *tree3;
-    char *str = NULL;
-    char    *s;
+	DIR	*dir;
 
-    tree3 = (t_exec *)tree;
-    executor_builtin(tree3, env_list);
-    dir = opendir(tree3->argv[0]);
-    if (dir)
-    {
-        closedir(dir);
-        errors(tree3->argv[0], 4);
-    }
-    if (ft_strchr(tree3->argv[0], '/'))
-    {   
-        if(access(tree3->argv[0], F_OK) != -1)
-            str = ft_strdup(tree3->argv[0]);
-        else
-            errors(tree3->argv[0], 4);
-    }
-    else
-    {
-        i = -1;
-        char    **path = envpath(*env_list);
-        while (path && path[++i])
-        {
-            s  = ft_strjoin3(ft_strjoin(path[i], "/"), tree3->argv[0]);
-            if (access(s, F_OK) != -1)
-                str = ft_strdup(s);
-            free(s);
-        }
-        if (!str)
-                str = ft_strdup(tree3->argv[0]);
-    }
-    execve(str, tree3->argv, list_to_table(*env_list));
-    free(str);
-    errors(tree3->argv[0], 0);
+	dir = opendir(s);
+	if (dir)
+	{
+		closedir(dir);
+		errors(s, 4);
+	}
+}
+
+char	*relative_path(t_exec *tree3, t_envvar **env_list)
+{
+	char	*str;
+	char	*s;
+	int		i;
+	char	**path;
+
+	i = -1;
+	str = NULL;
+	path = envpath(*env_list);
+	while (path && path[++i])
+	{
+		s = ft_strjoin3(ft_strjoin(path[i], "/"), tree3->argv[0]);
+		if (access(s, F_OK) != -1)
+			str = ft_strdup(s);
+		free(s);
+	}
+	if (!str)
+		str = ft_strdup(tree3->argv[0]);
+	return (str);
+}
+
+char	*get_path(t_exec *tree3, t_envvar **env_list)
+{
+	char	*str;
+
+	str = NULL;
+	if (ft_strchr(tree3->argv[0], '/'))
+	{
+		if (access(tree3->argv[0], F_OK) != -1)
+			str = ft_strdup(tree3->argv[0]);
+		else
+			errors(tree3->argv[0], 4);
+	}
+	else
+		str = relative_path(tree3, env_list);
+	return (str);
+}
+
+void	ft_exec(t_cmd *tree, t_envvar **env_list)
+{
+	t_exec	*tree3;
+	char	*str;
+
+	tree3 = (t_exec *)tree;
+	executor_builtin(tree3, env_list);
+	check_for_dir(tree3->argv[0]);
+	str = get_path(tree3, env_list);
+	execve(str, tree3->argv, list_to_table(*env_list));
+	free(str);
+	errors(tree3->argv[0], 0);
 }
--- executor.c
+++ executor.c
@@ -6,51 +6,51 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 13:00:15 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/10/15 01:19:49 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/15 01:51:11 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void check_in_files(t_cmd *redir)
+void	check_in_files(t_cmd *redir)
 {
-    t_redir   *tmp;
-    int     fd;
+	t_redir	*tmp;
+	int		fd;
 
-        tmp = (t_redir *)redir;
-        fd = open(tmp->file, tmp->mode);
-        if (fd < 0)
-        { 
-            close(fd);
-            if (!access(tmp->file, F_OK))
-                errors(tmp->file, 2);
-            else
-                errors(tmp->file, 3);
-        }
-        close(fd);
+	tmp = (t_redir *)redir;
+	fd = open(tmp->file, tmp->mode);
+	if (fd < 0)
+	{
+		close(fd);
+		if (!access(tmp->file, F_OK))
+			errors(tmp->file, 2);
+		else
+			errors(tmp->file, 3);
+	}
+	close(fd);
 }
 
-void find_in_redir(t_cmd *tree)
+void	find_in_redir(t_cmd *tree)
 {
-    t_redir  *tree2;
+	t_redir	*tree2;
 
-    if (tree->type == REDIR)
-    {
-        tree2 = (t_redir *)tree;
-        find_in_redir(tree2->cmd);
-        if (tree2->fd == 0)
-           check_in_files(tree); 
-    }
+	if (tree->type == REDIR)
+	{
+		tree2 = (t_redir *)tree;
+		find_in_redir(tree2->cmd);
+		if (tree2->fd == 0)
+			check_in_files(tree);
+	}
 }
 
-void    executor(t_cmd *tree, int *flag_out, int *flag_in, t_envvar **env_list)
+void	executor(t_cmd *tree, int *flag_out, int *flag_in, t_envvar **env_list)
 {
-    if (tree->type != PIPE)
-        find_in_redir(tree);
-    if (tree->type == PIPE)
-        ft_pipe(tree, flag_out, flag_in, env_list);
-    else if (tree->type == REDIR)
-        ft_redir(tree, flag_out, flag_in, env_list);
-    else
-        ft_exec(tree, env_list);
+	if (tree->type != PIPE)
+		find_in_redir(tree);
+	if (tree->type == PIPE)
+		ft_pipe(tree, flag_out, flag_in, env_list);
+	else if (tree->type == REDIR)
+		ft_redir(tree, flag_out, flag_in, env_list);
+	else
+		ft_exec(tree, env_list);
 }
