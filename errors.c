74f299337a7484bf8ba23525b3acd507c3275a5c
hassan khalil <hkhalil@student.1337.ma>
2022-10-17 03:23:07 +0100

    fixed redirections in builtins

--- errors.c
+++ errors.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 20:13:17 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/10/14 20:54:47 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/17 03:06:27 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,23 @@ void	errors3(char *name)
 		exit(126);
 }
 
+void	errors4(char *name, int flag)
+{
+	write(2, name, ft_strlen(name));
+	if (flag == 10)
+		write(2, ": Permission denied\n", 21);
+	else
+		write(2, ": No such file or directory\n", 29);
+	g_var = 1;
+}
+
 void	errors(char *name, int flag)
 {
+	if (flag == 10 || flag == 11)
+	{
+		errors4(name, flag);
+		return ;
+	}
 	if (flag == 4 && errno != 2)
 		errors1(name);
 	if (flag == 2 || flag == 3)
--- parent_builtin.c
+++ parent_builtin.c
@@ -6,11 +6,57 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 20:41:25 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/10/15 21:12:15 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/17 03:18:02 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	check_in_files0(t_cmd *redir, int *flag)
+{
+	t_redir	*tmp;
+	int		fd;
+
+	tmp = (t_redir *)redir;
+	fd = open(tmp->file, tmp->mode);
+	if (fd < 0)
+	{
+		close(fd);
+		if (!access(tmp->file, F_OK))
+			errors(tmp->file, 10);
+		else
+			errors(tmp->file, 11);
+		*flag = 1;
+	}
+	close(fd);
+}
+
+void	find_in_redir0(t_cmd *tree, int *flag)
+{
+	t_redir	*tree2;
+
+	if (tree->type == REDIR)
+	{
+		tree2 = (t_redir *)tree;
+		find_in_redir0(tree2->cmd, flag);
+		if (tree2->fd == 0)
+			check_in_files0(tree, flag);
+	}
+}
+
+void	open_files(t_cmd *tree, t_envvar **env_list)
+{
+	t_redir	*tree2;
+	int		open_fd;
+
+	tree2 = (t_redir *)tree;
+	open_fd = open(tree2->file, tree2->mode, 0666);
+	if (open_fd < 0)
+		return ;
+	close(open_fd);
+	if (tree2->cmd->type == REDIR)
+		open_files(tree2->cmd, env_list);
+}
 
 int	parent_builtin_exit(t_cmd *tree)
 {
@@ -63,15 +109,15 @@ int	parent_builtin_export(t_cmd *tree, t_envvar **env)
 	t_exec	*cmd;
 
 	cmd = (t_exec *)tree;
-	if (!ft_strcmp(cmd->argv[0], "export"))
+	if (!ft_strcmp(cmd->argv[0], "export") && cmd->argv[1])
 	{
-		ft_export(cmd, env);
-		if (g_var == -2)
-			g_var = 1;
-		else
-			g_var = 0;
-		clean(tree);
-		return (1);
+			ft_export(cmd, env);
+			if (g_var == -2)
+				g_var = 1;
+			else
+				g_var = 0;
+			clean(tree);
+			return (1);
 	}
 	return (0);
 }
@@ -79,12 +125,22 @@ int	parent_builtin_export(t_cmd *tree, t_envvar **env)
 int	parent_builtin(char *buff, t_envvar **env)
 {
 	t_cmd	*tree;
-	t_exec	*cmd;
+	t_cmd	**addr;
+	int 	flag = 0;
 
 	tree = parsecmd(buff, env, 0);
-	if (tree->type == EXEC)
+	addr = &tree;
+	if (tree->type != PIPE)
 	{
-		cmd = (t_exec *)tree;
+		//check builtin
+		find_in_redir0(tree, &flag);
+		if (flag)
+			return (1);
+		if (tree->type == REDIR)
+			open_files(tree, env);
+		tree = *addr;
+		while (tree->type == REDIR)
+			tree = ((t_redir *)tree)->cmd;
 		if (parent_builtin_exit(tree))
 			return (1);
 		else if (parent_builtin_cd(tree, env))
