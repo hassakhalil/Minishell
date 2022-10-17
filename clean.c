88e0903526a7b4b2f2869b8f09191e547f438841
hassan khalil <hkhalil@student.1337.ma>
2022-10-17 09:46:31 +0100

    norm

--- clean.c
+++ clean.c
@@ -6,68 +6,69 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 20:31:23 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/10/17 05:06:09 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/17 09:46:00 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    free_env(t_envvar **env)
+void	free_env(t_envvar **env)
 {
-    while (env && *env)
-    {
-        free((*env)->name);
-        if ((*env)->value)
-            free((*env)->value);
-        *env = (*env)->next;
-    }
+	while (env && *env)
+	{
+		free((*env)->name);
+		if ((*env)->value)
+			free((*env)->value);
+		*env = (*env)->next;
+	}
 }
 
-void    free_exec(t_exec *node)
+void	free_exec(t_exec *node)
 {
-    int i = 0;
+	int	i;
 
-    while (node->argv[i])
-    {
-        free(node->argv[i]);
-        i++;
-    }
-    free(node);
+	i = 0;
+	while (node->argv[i])
+	{
+		free(node->argv[i]);
+		i++;
+	}
+	free(node);
 }
 
-void    free_redir(t_redir *node)
+void	free_redir(t_redir *node)
 {
-    free(node->file);
-    free(node);
+	free(node->file);
+	free(node);
 }
 
-void    free_pipe(t_pip *node)
+void	free_pipe(t_pip *node)
 {
-    free(node);
+	free(node);
 }
 
-void    clean(t_cmd *tree)
+void	clean(t_cmd *tree)
 {
-    t_pip     *tree1;
-    t_redir   *tree2;
-    t_exec    *tree3;
+	t_pip	*tree1;
+	t_redir	*tree2;
+	t_exec	*tree3;
 
-    if (tree->type == PIPE)
-    {
-        tree1 = (t_pip *)tree;
-        clean(tree1->left);
-        clean(tree1->right);
-        free_pipe(tree1);
-    }
-    else if (tree->type == REDIR)
-    {
-        tree2 = (t_redir *)tree;
-        clean(tree2->cmd);
-        free_redir(tree2);
-    }
-    else
-    {
-        tree3 = (t_exec *)tree;
-        free_exec(tree3);
-    }
+	if (tree->type == PIPE)
+	{
+		tree1 = (t_pip *)tree;
+		clean(tree1->left);
+		clean(tree1->right);
+		free_pipe(tree1);
+	}
+	else if (tree->type == REDIR)
+	{
+		tree2 = (t_redir *)tree;
+		clean(tree2->cmd);
+		free_redir(tree2);
+	}
+	else
+	{
+		tree3 = (t_exec *)tree;
+		free_exec(tree3);
+	}
 }
